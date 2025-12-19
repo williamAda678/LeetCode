56766585b43f93fe7a6ad5999752671f7f9e011b
williamAda678 <wadams678@gmail.com>
2025-12-19 19:18:17 +1100

    Time: 1 ms (99.73%) | Memory: 34.5 MB (82.25%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+       
+        if(x<0) return false;
+        
+        if(x %10 == 0&& x!= 0) return false;
+
+        int r =0;
+
+        while(x>r)
+        {
+            r = r *10 +x%10;
+            x /=10;
+        }
+        
+        return x== r|| x == r/10;
+    }
+}
