396052b83b66ada1b26d6c695171aeb07ef4b962
williamAda678 <wadams678@gmail.com>
2025-12-22 16:44:12 +1100

    Time: 0 ms (100.00%) | Memory: 53.1 MB (5.22%) - LeetSync

---
+++ 792-binary-search/binary-search.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+                int left = 0;
+        int right = nums.Length -1;
+
+        while(left <= right)
+        {
+            int middle = left + (right - left) /2;
+            if(nums[middle] == target) return middle;
+            else if(nums[middle] < target) left = middle + 1;
+            else right = middle -1;
+        }
+
+        return -1;
+    }
+}
