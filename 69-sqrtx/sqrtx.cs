274e75017fa2349e8db242622813fcea3be7085f
williamAda678 <wadams678@gmail.com>
2026-01-03 15:32:00 +1100

    Time: 0 ms (100.00%) | Memory: 29 MB (82.48%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MySqrt(int x) {
+        if( x == 1) return x;
+       int left =1;
+       int right = x/2;
+
+       while(left <= right)
+       {
+        int mid = left + (right - left)/2;
+
+        if(mid == x / mid) return mid;
+        else if(mid < x / mid) left = mid +1;
+        else right = mid -1;
+       }
+
+        return right;
+    }
+}
