0ef1a1e8161fa574dbf4b731f949183fbcc3be9f
williamAda678 <wadams678@gmail.com>
2026-01-03 18:01:01 +1100

    Time: 0 ms (100.00%) | Memory: 50.6 MB (34.09%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cs
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cs
@@ -14,6 +14,34 @@
 public class Solution {
     public int CountNodes(TreeNode root) {
         if(root == null) return 0;
+        int leftNodeDepth = CountLeftNodes(root);
+        int rightNodeDepth = CountRightNodes(root);
+
+        if(leftNodeDepth == rightNodeDepth)
+        {    
+            return (int) Math.Pow(2, leftNodeDepth) -1;
+        }
+
         return  1 + CountNodes(root.left) + CountNodes(root.right);
+    }
+
+    public int CountLeftNodes(TreeNode root) {
+        int count =0;
+        while(root != null)
+        {
+            root = root.left;
+            count++;
+        }
+        return  count;
+    }
+
+    public int CountRightNodes(TreeNode root) {
+        int count =0;
+        while(root != null)
+        {
+            root = root.right;
+            count++;
+        }
+        return  count;
     }
 }
